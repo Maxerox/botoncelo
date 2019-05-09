e06fa421db6929b0ca7dd087d020187b411c058c
Cristian Conedera <cristian.conedera@bothmedia.com>
2019-05-08 21:51:32 -0300

    Updates

---
+++ precache-manifest.29687913fd8a489568a28dcbc07c1659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd009eb23241bdf9ec9b15ace8d0a505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb82a47035df04aeb283",
+    "url": "/static/css/main.59432b7a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bb82a47035df04aeb283",
+    "url": "/static/js/main.b11cc41f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b2b904100aa1593e68e48e087332432a",
+    "url": "/static/media/marce.b2b90410.png"
+  }
+]);
