82ff80bf7555a36dd3be01c274ce5a163c075464
Cristian Conedera <cristian.conedera@bothmedia.com>
2020-09-18 10:29:24 -0300

    Botoncelo v2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import share from './images/share.svg';
 import './App.css';
 
 const sounds = [
-  { src: './sounds/falopa.mp3', title: 'Falopa' },
-  { src: './sounds/famoso.mp3', title: 'Famoso' },
-  { src: './sounds/mechupa.mp3', title: 'Me chupa...' },
-  { src: './sounds/pingo.mp3', title: 'Pingo' },
-  { src: './sounds/sos-bastante-puto.mp3', title: 'Sos bastante...' },
-  { src: './sounds/enamorar.ogg', title: 'Enamorado' },
-  { src: './sounds/manga-de-pajeros.mp3', title: 'Manga de pajeros...' },
-  { src: './sounds/sisisi.mp3', title: 'Si si si' },
-  { src: './sounds/pija.ogg', title: 'Pija' },
-  { src: './sounds/te-la-puso.mp3', title: 'Te la puso' },
-  { src: './sounds/az-querido.opus', title: 'Az querido!', extra: true },
+  { src: 'sounds/falopa.mp3', title: 'Falopa' },
+  { src: 'sounds/famoso.mp3', title: 'Famoso' },
+  { src: 'sounds/mechupa.mp3', title: 'Me chupa...' },
+  { src: 'sounds/pingo.mp3', title: 'Pingo' },
+  { src: 'sounds/sos-bastante-puto.mp3', title: 'Sos bastante...' },
+  { src: 'sounds/enamorar.ogg', title: 'Enamorado' },
+  { src: 'sounds/manga-de-pajeros.mp3', title: 'Manga de pajeros...' },
+  { src: 'sounds/sisisi.mp3', title: 'Si si si' },
+  { src: 'sounds/pija.ogg', title: 'Pija' },
+  { src: 'sounds/te-la-puso.mp3', title: 'Te la puso' },
+  { src: 'sounds/az-querido.opus', title: 'Az querido!', extra: true },
 ]
 
 
@@ -43,11 +43,33 @@ class SoundButton extends React.Component {
     //this.sound.pause();
   }
 
+  async handleShare() {
+    const audioRequest = await fetch(this.props.sound.src);
+    const audioBlob = await audioRequest.blob();
+    const audioFile = new File([audioBlob], 'botoncelo.mp3', { type: 'audio/mp3' });
+    
+    console.log(audioFile)
+    if (navigator.canShare && navigator.canShare({ files: [audioFile] })) {
+      navigator.share({
+        files: [audioFile],
+        title: 'Pictures',
+        text: 'Our Pictures.',
+      })
+      .then(() => console.log('Share was successful.'))
+      .catch((error) => console.log('Sharing failed', error));
+    } else {
+      console.log(`Your system doesn't support sharing files.`);
+    }
+  }
+
   render() {
     return (
       <div className="sound-box">
         <audio ref={(el) => this.sound = el } src={this.props.sound.src} controls preload="true" />
         <button className="sound-button" onMouseDown={(e) => this.handleMouseDown(e)} onMouseUp={(e) => this.handleMouseUp(e)}>{this.props.sound.title}</button>
+        <button className="share-button" onClick={(e) => this.handleShare(e)}>
+          <img className="share-img" src={share} />
+        </button>
       </div>
     ) 
   }
